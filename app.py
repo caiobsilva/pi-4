5a18293f902316652617c4491caae368c09a26fd
Juan Suman <victor.suman23@gmail.com>
2019-12-03 19:07:38 -0300

    Atualização final.

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 from pi4 import gerar_mapa_gasto
+from pi4 import gerar_mapa_fundos
 
 app = Flask(__name__)
 
@@ -8,17 +9,23 @@ def before_request():
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+        return render_template('index.html')
 
-@app.route('/map')
-def map():
-    return render_template('head-map.html')
+@app.route('/map-gastos')
+def mapGastos():
+        gerar_mapa_gasto(1994)
+        return render_template('head-map-gasto.html')
 
-@app.route('/foo', methods=['GET', 'POST'])
-def foo(anos=[]):
+@app.route('/map-fundos')
+def mapFundos():
+        gerar_mapa_fundos(2008)
+        return render_template('head-map-fundo.html')
+
+@app.route('/gastos', methods=['GET', 'POST'])
+def gastos(anos=[]):
         ano = 0
         if request.method == "GET":
-                return render_template("head-map.html", anos=anos)
+                return render_template("head-map-gasto.html", anos=anos)
 
         #recebe o value do botão de submit. 
         ano = int(request.form.get('text_input', None))
@@ -30,7 +37,25 @@ def foo(anos=[]):
         anos.append(ano)
 
         print("Atualizando mapa para: " + str(ano))
-        return redirect(url_for('foo'))
+        return redirect(url_for('gastos'))
+
+@app.route('/fundos', methods=['GET', 'POST'])
+def fundos(anos=[]):
+        ano = 0
+        if request.method == "GET":
+                return render_template("head-map-fundo.html", anos=anos)
+
+        #recebe o value do botão de submit. 
+        ano = int(request.form.get('text_input', None))
+
+        #gera o mapa coroplético de fundos.
+        gerar_mapa_fundos(ano)
+
+        #log de anos
+        anos.append(ano)
+
+        print("Atualizando mapa para: " + str(ano))
+        return redirect(url_for('fundos'))
 
 if __name__ == '__main__':
     #app.before_request(before_request)
